3560326729a1733612159eb25fd0c80ab874d202
danisz118 <daniz118@gmail.com>
2021-12-18 13:25:45 +0200

    hw 11

---
+++ src/main/java/LocalDateDeserializer.java
@@ -0,0 +1,20 @@
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+
+    public class LocalDateDeserializer extends StdDeserializer<LocalDate> {
+        public LocalDateDeserializer() {
+            super(LocalDate.class);
+        }
+
+        @Override
+        public LocalDate deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+            return LocalDate.parse(p.readValueAs(String.class));
+        }
+    }
+
---
+++ src/main/java/LocalDateTimeDeserializer.java
@@ -0,0 +1,17 @@
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+public class LocalDateTimeDeserializer extends StdDeserializer<LocalDateTime> {
+    public LocalDateTimeDeserializer(){
+        super(LocalDateTime.class);
+    }
+    @Override
+    public LocalDateTime deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+        return LocalDateTime.parse(p.readValueAs(String.class));
+    }
+}
